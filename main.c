2ffaf8bdd291b36ed50cb5f35ae4cbc7c4d250cf
Narita1910 <43438380+Narita1910@users.noreply.github.com>
2018-09-20 11:38:05 +0000

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,8 @@ int main(int argc, char *argv[])
 {
     char str[256];
     fgets(str, sizeof(str), stdin);
-    printf("%s\n", "XXXXX");
+    scanf("%s",str);
+    printf("%s\n", str);
 
     return 0;
 }
