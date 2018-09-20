2c755132ada294deaa2a6fa8825b79eca94d2c2c
oonuki1030 <43439440+oonuki1030@users.noreply.github.com>
2018-09-20 20:41:38 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(int argc, char *argv[])
 {
     char str[256];
     fgets(str, sizeof(str), stdin);
-    printf("%s\n", "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!");
+    printf("%s\n", "input.txt");
 
     return 0;
 }
