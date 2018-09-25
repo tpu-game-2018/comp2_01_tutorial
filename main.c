159d851f14b3632f22b8cb7e17462fbbcb4af40b
1827010 <43464024+1827010@users.noreply.github.com>
2018-09-25 03:42:21 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,23 @@
-#include <stdio.h>
+       #include <stdio.h>
+　      #inclide <stdlib.h>
 
-int main(int argc, char *argv[]) 
-{
-    char str[256];
-    fgets(str, sizeof(str), stdin);
-    printf("%s\n", "XXXXX");
-
-    return 0;
+　      int main(int argc, char *argv[]) 
+　     {   
+        FILE *fp
+       char *filename = argv[1];
+        if ((fp = fopen(filename,"r")) == NULL){
+       fprint(stderr,"Failed to open %s.\n",filename);
+            exit(EXIT_FAILURE);
+          }
+         char str［２５６］
+ 
+   　　　fgets(str,sizeof(str),fp);
+        printf("%s\n,"XXXXX");
+          
+       fclose(fp);              
+       fgets(str, sizeof(str), stdin);
+       print("%s\n",str);
+          
+      return 0;
+          //答えらしきソースをがっつり参考にしました。
 }
