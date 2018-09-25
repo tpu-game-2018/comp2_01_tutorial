90b0a95c027910f4ec47d64b6a048c0b17323914
1827010 <43464024+1827010@users.noreply.github.com>
2018-09-25 16:58:06 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,10 @@
        #include <stdio.h>
-　      #include <stdlib.h>
 
-　      int main(int argc, char *argv[]) 
-　     {   
-        FILE *fp;
-       char *filename = argv[1];
-        if ((fp = fopen(filename,"r")) == NULL){
-       fprint(stderr,"Failed to open %s.\n",filename);
-            exit(EXIT_FAILURE);
-          }
-         char str [256];
- 
-   　　　fgets(str,sizeof(str),fp);
-        printf("%s\n,"XXXXX");
-          
-       fclose(fp);              
-       fgets(str, sizeof(str), stdin);
-       print("%s\n",str);
-          
-      return 0;
-          
+int main(int argc, char *argv[]) 
+{
+    char str[256];
+    fgets(str, sizeof(str), stdin);
+    printf("%s\n", str);
+
+    return 0;
 }
