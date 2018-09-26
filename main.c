e1cd33f11d848530ec28c4e59f3760628041bb3a
chieri611 <43595660+chieri611@users.noreply.github.com>
2018-09-26 14:26:11 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(int argc, chsr *argv[])
     char str[256]
         fgets(str,sizeof(str),stdin);
     
-    printf("\n\n出力↓\n");
+    printf("\n\n\n出力↓\n");
     printf("%s\n",str);
     
     return 0;
