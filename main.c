842d6977658209f86d6f3a8f59eb27e4dbf4f059
rui0926 <43432504+rui0926@users.noreply.github.com>
2018-09-26 13:36:35 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,8 +4,7 @@ int main(int argc, char *argv[])
 {
     char str[256];
     fgets(str, sizeof(str), stdin);
-    scanf("%s\n",str);
-    printf("%s", str);
+    printf("%s/n", str);
 
     return 0;
 }
